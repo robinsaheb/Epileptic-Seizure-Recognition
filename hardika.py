f1beab6f5860c5ff0e4ba5285a37140c7fed04cc
Saheb Singh <sebs1797@gmail.com>
2018-03-20 08:41:13 +0530

    Initial Commit

---
+++ hardika.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 19 21:39:46 2018
+
+@author: sahebsingh
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+dataset = pd.read_csv('data.csv')
+X = dataset.iloc[:, 1:179].values
+y = dataset.iloc[:, 179:].values
+
+for i in range(len(y)):
+    if y[i] == 1:
+        y[i] = 1
+    else:
+        y[i] = 0
+
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+#Making the ANN
+
+#Importing keras libraries and packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+#Initializing the ANN
+classifier = Sequential()
+
+#Adding input layer and first hidden layer
+classifier.add(Dense(output_dim = 80, init = 'uniform', activation = 'relu', input_dim = 178))
+
+#Adding second hidden layer
+classifier.add(Dense(output_dim = 80, init = 'uniform', activation = 'relu'))
+
+#Adding the output layer
+classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+
+#Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+#Fitting the ANN to the training set
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 1)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+# K Fold Validation 
+from sklearn.model_selection import StratifiedKFold
+seed = 7
+np.random.seed(seed)
+kfold = StratifiedKFold(n_splits = 5, shuffle = True, random_state = seed)
+cvscores = []
+for train, test in kfold.split(X, y):
+    # Create model
+	model = Sequential()
+	model.add(Dense(output_dim = 80, init = 'uniform', activation = 'relu', input_dim = 178))
+	model.add(Dense(output_dim = 80, init = 'uniform', activation = 'relu'))
+	model.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+	# Compile model
+	model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+	# Fit the model
+	model.fit(X_train, y_train, epochs=15, batch_size=10, verbose=0)
+	# Evaluate the model
+	scores = model.evaluate(X_test, y_test, verbose=0)
+	print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+	cvscores.append(scores[1] * 100)
+print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
